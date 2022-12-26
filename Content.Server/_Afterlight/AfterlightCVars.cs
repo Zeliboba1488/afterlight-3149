d45a6d52214a295cffbdde021c87ee823b1ee0eb
Moony <moony@hellomouse.net>
2022-12-26 17:45:26 -0600

    Al story flags (#11)
    Handles saving and restoring the game's story.
    
    Co-authored-by: moonheart08 <moonheart08@users.noreply.github.com>

--- Content.Server/_Afterlight/AfterlightCVars.cs
+++ Content.Server/_Afterlight/AfterlightCVars.cs
@@ -6,9 +6,26 @@ namespace Content.Server._Afterlight;
 public sealed class AfterlightCVars
 {
     /// <summary>
-    /// Whether or not world generation is enabled.
+    /// Whether or not ship spawning is enabled.
     /// </summary>
     public static readonly CVarDef<bool> ShipSpawningEnabled =
         CVarDef.Create("afterlight.shipspawning.enabled", true, CVar.SERVERONLY);
 
+    /// <summary>
+    /// Whether or not story management is enabled.
+    /// </summary>
+    public static readonly CVarDef<bool> StoryEnabled =
+        CVarDef.Create("afterlight.story_enabled", true, CVar.SERVERONLY);
+
+    /// <summary>
+    /// The path to save the game's story data to.
+    /// </summary>
+    public static readonly CVarDef<string> StoryMapPath =
+        CVarDef.Create("afterlight.story_map_path", "/storydata.yml", CVar.SERVERONLY);
+
+    /// <summary>
+    /// The path to save backups of the story data to.
+    /// </summary>
+    public static readonly CVarDef<string> StoryBackupMapPath =
+        CVarDef.Create("afterlight.story_map_backup_path", "/storyBack/storydata.yml", CVar.SERVERONLY);
 }
---
+++ Content.Server/_Afterlight/Story/StoryManagementSystem.cs
@@ -0,0 +1,55 @@
+﻿using System.Globalization;
+using Content.Server.Chat.Managers;
+using Content.Server.GameTicking;
+using Robust.Server.GameObjects;
+using Robust.Shared.Configuration;
+using Robust.Shared.Map;
+
+namespace Content.Server._Afterlight.Story;
+
+/// <summary>
+/// This handles the game's story and it's persistence.
+/// </summary>
+public sealed class StoryManagementSystem : EntitySystem
+{
+    [Dependency] private readonly MapLoaderSystem _mapLoader = default!;
+    [Dependency] private readonly IConfigurationManager _cfg = default!;
+    [Dependency] private readonly IMapManager _map = default!;
+    [Dependency] private readonly IChatManager _chatManager = default!;
+
+    private MapId? _storyMap;
+
+    /// <inheritdoc/>
+    public override void Initialize()
+    {
+        SubscribeLocalEvent<GameRunLevelChangedEvent>(OnRunLevelChanged);
+    }
+
+    private void OnRunLevelChanged(GameRunLevelChangedEvent ev)
+    {
+        if (!_cfg.GetCVar(AfterlightCVars.StoryEnabled))
+            return;
+
+        if (ev.New == GameRunLevel.InRound)
+        {
+            var newMap = _map.CreateMap();
+            _storyMap = newMap;
+            if (!_mapLoader.TryLoad(newMap, _cfg.GetCVar(AfterlightCVars.StoryMapPath), out _))
+            {
+                _chatManager.DispatchServerAnnouncement("Failed to load the game's story data. Please contact your server administrator!");
+#if DEBUG
+                _chatManager.DispatchServerAnnouncement("(Or if you're doing local development, ignore this message.)");
+#endif
+            }
+        }
+        else if (ev.Old == GameRunLevel.InRound && _storyMap is not null)
+        {
+            // Save the game if we leave round for any reason.
+            _mapLoader.SaveMap(_storyMap.Value, _cfg.GetCVar(AfterlightCVars.StoryMapPath));
+            // For good measure, do it again at a backup location.
+            _mapLoader.SaveMap(_storyMap.Value, $"{_cfg.GetCVar(AfterlightCVars.StoryBackupMapPath)}_{DateTime.Now.ToString(CultureInfo.InvariantCulture)}");
+
+            _storyMap = null;
+        }
+    }
+}
