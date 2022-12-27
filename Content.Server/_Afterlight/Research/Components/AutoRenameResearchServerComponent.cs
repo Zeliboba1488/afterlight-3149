78fe885bd3b4ea0f0f3a33c07393d1942bcaa86c
moonheart08 <moonheart08@users.noreply.github.com>
2022-12-27 11:49:48 -0600

    Makes the R&D server automatically rename to match it's parent station.

---
+++ Content.Server/_Afterlight/Research/Components/AutoRenameResearchServerComponent.cs
@@ -0,0 +1,9 @@
+﻿namespace Content.Server._Afterlight.Research.Components;
+
+/// <summary>
+/// This is used for automatically renaming the R&D server. It does nothing else.
+/// </summary>
+[RegisterComponent]
+public sealed class AutoRenameResearchServerComponent : Component
+{
+}
---
+++ Content.Server/_Afterlight/Research/Systems/AutoRenameResearchServerSystem.cs
@@ -0,0 +1,39 @@
+﻿using Content.Server._Afterlight.Research.Components;
+using Content.Server.Station.Systems;
+using Content.Shared.Research.Components;
+
+namespace Content.Server._Afterlight.Research.Systems;
+
+/// <summary>
+/// This handles renaming the R&D server automatically.
+/// </summary>
+public sealed class AutoRenameResearchServerSystem : EntitySystem
+{
+    [Dependency] private readonly StationSystem _station = default!;
+
+    /// <inheritdoc/>
+    public override void Initialize()
+    {
+        SubscribeLocalEvent<AutoRenameResearchServerComponent, MapInitEvent>(OnMapInit);
+        SubscribeLocalEvent<TryRenameEvent>(OnTryRename);
+    }
+
+    private void OnTryRename(TryRenameEvent ev)
+    {
+        var station = _station.GetOwningStation(ev.Target);
+        if (station is null)
+            return;
+        var server = Comp<ResearchServerComponent>(ev.Target);
+        server.ServerName = $"RDSERVER ({Name(station.Value)})";
+    }
+
+    private void OnMapInit(EntityUid uid, AutoRenameResearchServerComponent component, MapInitEvent args)
+    {
+        QueueLocalEvent(new TryRenameEvent() { Target = uid });
+    }
+
+    private sealed class TryRenameEvent : EntityEventArgs
+    {
+        public required EntityUid Target;
+    }
+}
