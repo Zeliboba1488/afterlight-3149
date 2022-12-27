ac15f25b7952b8a5411857ad69d4f65bc3855931
moonheart08 <moonheart08@users.noreply.github.com>
2022-12-27 12:49:25 -0600

    save

---
+++ Content.Server/_Afterlight/StarCharts/Components/StarChartableComponent.cs
@@ -0,0 +1,19 @@
+﻿using Robust.Shared.Map;
+
+namespace Content.Server._Afterlight.StarCharts.Components;
+
+/// <summary>
+/// This is used for entities that should be findable with a redeemed star chart.
+/// </summary>
+[RegisterComponent]
+public sealed class StarChartableComponent : Component
+{
+    [DataField("chartedLocation")]
+    public MapCoordinates? ChartedLocation;
+
+    [DataField("chartName", required: true)]
+    public string ChartName = default!;
+
+    [DataField("chartDescription", required: true)]
+    public string ChartDescription = default!;
+}
---
+++ Content.Server/_Afterlight/StarCharts/Systems/StarChartableSystem.cs
@@ -0,0 +1,13 @@
+﻿namespace Content.Server._Afterlight.StarCharts.Systems;
+
+/// <summary>
+/// This handles star charts,
+/// </summary>
+public sealed class StarChartableSystem : EntitySystem
+{
+    /// <inheritdoc/>
+    public override void Initialize()
+    {
+
+    }
+}
