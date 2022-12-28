25340c83cc9b7555caa3daa3f077ae0a45c41f46
moonheart08 <moonheart08@users.noreply.github.com>
2022-12-27 22:49:07 -0600

    fiberous...

---
+++ Content.Server/_Afterlight/Components/FiberousComponent.cs
@@ -0,0 +1,9 @@
+﻿namespace Content.Server._Afterlight.Components;
+
+/// <summary>
+/// This is used for any fiberous plant to allow it to be converted into cloth.
+/// </summary>
+[RegisterComponent]
+public sealed class FiberousComponent : Component
+{
+}
