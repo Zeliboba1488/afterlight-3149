6c07b7af2a45a9bdf5ba6e7a245bd56208130e28
Moony <moony@hellomouse.net>
2023-02-20 23:51:31 -0600

    Upstream merge 02 20 23 (#41)
    .
    
    ---------
    
    Co-authored-by: eclips_e <67359748+Just-a-Unity-Dev@users.noreply.github.com>
    Co-authored-by: Kara <lunarautomaton6@gmail.com>
    Co-authored-by: Kevin Zheng <kevinz5000@gmail.com>
    Co-authored-by: Joel Hedtjärn <joel.hedtjarn@gmail.com>
    Co-authored-by: PJBot <pieterjan.briers+bot@gmail.com>
    Co-authored-by: Emisse <99158783+Emisse@users.noreply.github.com>
    Co-authored-by: Scribbles0 <91828755+Scribbles0@users.noreply.github.com>
    Co-authored-by: metalgearsloth <31366439+metalgearsloth@users.noreply.github.com>
    Co-authored-by: lunar-crater-ex <112364074+lunar-crater-ex@users.noreply.github.com>
    Co-authored-by: moonheart08 <moonheart08@users.noreply.github.com>

--- Tools/upstream_merge_tool.ps1
+++ Tools/upstream_merge_tool.ps1
@@ -1,6 +1,6 @@
 ﻿Write-Output "Moony's upstream merge workflow tool."
 Write-Output "This tool can be stopped at any time, i.e. to finish a merge or resolve conflicts. Simply rerun the tool after having resolved the merge with normal git cli."
-Write-Output "Pay attention to any output from git!"
+Write-Output "Pay attention to any output from git! DO NOT RUN THIS ON A WORKING TREE WITH UNCOMMITTED FILES OF ANY KIND."
 $target = Read-Host "Enter the branch you're syncing toward (typically upstream/master or similar)"
 $refs = git log --reverse --format=format:%H HEAD.. $target
 
@@ -10,29 +10,80 @@ $skipOption = New-Object System.Management.Automation.Host.ChoiceDescription "&S
 
 $mergeOptions = [System.Management.Automation.Host.ChoiceDescription[]]($skipOption, $mergeOption, $cherryPickOption)
 
+$nonlinears = @()
+
 foreach ($unmerged in $refs) {
+    # Finding non-linear commits i.e. merges..
+    $parents = (git log --format=format:%P -n 1 $unmerged) -split '\s+'
+    if ($parents.Length -eq 1) {
+        continue
+    }
+
+    # And indexing them, as we're going to need to skip them later to pick the actual merge.
+    $nonlinears = $nonlinears + $parents[1..($parents.Length-1)]
+}
+
+foreach ($unmerged in $refs) {
+    if ($nonlinears -contains $unmerged) {
+        Write-Output ("Skipping over {0}, which we'll merge later (non-linear history encountered)." -f $unmerged)
+        continue
+    }
+
     $summary = git show --format=format:%s $unmerged
 
     if ($summary -ieq "automatic changelog update") {
         Write-Output ("Deliberately skipping changelog bot commit {0}." -f $unmerged)
+        Write-Output "== GIT (CONFLICTS ARE OKAY) =="
+        git merge --no-ff --no-commit --no-verify $unmerged
+        # DELIBERATELY IGNORE merge conflict markers. We're just going to undo the commit!
+        git add *
+        git commit -m ("squash! Merge tool skipping '{0}'" -f $unmerged)
+        $newhead = git log -n 1 --format=format:%H
+        git reset HEAD~ --hard
+        git reset $newhead --soft
+        git commit --amend --no-edit
+        Write-Output "== DONE =="
         continue
     }
 
     git show --format=full --summary $unmerged
 
+    $parents = (git log --format=format:%P -n 1 $unmerged) -split '\s+'
+    Write-Output $parents
+
+    if ($parents.Length -ne 1) {
+        $mergedin = $parents[1..($parents.Length-1)]
+        Write-Output "Which has children (note: Merging again will create a tower of merges, but fully preserves history):"
+        foreach ($tomerge in $mergedin) {
+            git show --format=full --summary $mergedin
+        }
+    }
+
     $response = $host.UI.PromptForChoice("Commit action?", "", $mergeOptions, 0)
 
     Switch ($response) {
-        0 {
+        2 {
             Write-Output "== GIT =="
-            git cherry-pick $unmerged
+            git cherry-pick -m 1 --allow-empty $unmerged
             Write-Output "== DONE =="
         }
         1 {
             Write-Output "== GIT =="
-            git merge $unmerged
+            git merge --no-ff -m ("squash! Merge tool integrating '{0}'" -f $unmerged) $unmerged
             Write-Output "== DONE =="
         }
-        2 { Write-Output ("Skipping " -f $unmerged) }
+        0 {
+            Write-Output ("Skipping {0}" -f $unmerged)
+            Write-Output "== GIT (CONFLICTS ARE OKAY) =="
+            git merge --no-ff --no-commit --no-verify $unmerged
+            # DELIBERATELY IGNORE merge conflict markers. We're just going to undo the commit!
+            git add *
+            git commit -m ("squash! Merge tool skipping '{0}'" -f $unmerged)
+            $newhead = git log -n 1 --format=format:%H
+            git reset HEAD~ --hard
+            git reset $newhead --soft
+            git commit --amend --no-edit
+            Write-Output "== DONE =="
+        }
     }
 }
